7ab7f8d041f07fe3fe747da2a279a1ac321dfdfd
James Simpson <edenofest@gmail.com>
2022-03-05 18:27:04 -0500

    feat(api): Construct a skeleton api and override store

---
+++ lua/nightfox/init.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+local override = {}
+function override.pallets(pallets)
+  require("nightfox.override").pallets = pallets
+end
+
+function override.schemes(schemes)
+  require("nightfox.override").schemes = schemes
+end
+
+function override.groups(groups)
+  require("nightfox.override").groups = groups
+end
+
+M.override = override
+
+function M.init(opts) end
+
+function M.setup(opts)
+  if opts.options then
+    M.init(opts.options)
+  end
+
+  if opts.pallets then
+    M.override.pallets(opts.pallets)
+  end
+
+  if opts.schemes then
+    M.override.schemes(opts.schemes)
+  end
+
+  if opts.groups then
+    M.override.groups(opts.groups)
+  end
+end
+
+return M
---
+++ lua/nightfox/override.lua
@@ -0,0 +1,21 @@
+local collect = require("nightfox.lib.collect")
+
+local store = {
+  pallets = {},
+  schemes = {},
+  groups = {},
+}
+
+return setmetatable({}, {
+  __index = function(_, value)
+    if store[value] then
+      return store[value]
+    end
+  end,
+
+  __newindex = function(_, key, value)
+    if store[key] then
+      store[key] = collect.deep_extend(store[key], value or {})
+    end
+  end,
+})
