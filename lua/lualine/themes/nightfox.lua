933b7d44297f9fb20571aeb4540513d03620636c
James Simpson <edenofest@gmail.com>
2022-03-07 21:47:13 -0500

    feat(statusline): Add lualine

---
+++ lua/lualine/themes/nightfox.lua
@@ -0,0 +1,31 @@
+local name = require('nightfox.config').fox
+local spec = require('nightfox.spec').load(name)
+
+local pal = spec.pallet
+local bg = spec.bg0
+local fg = spec.fg2
+
+local function generate_mode(color, amount)
+  amount = amount or 0.3
+  local fade = bg:blend(color, amount):to_css()
+  local b = bg:to_css()
+  local f = fg:to_css()
+
+  return {
+    a = { bg = color:to_css(), fg = b },
+    b = { bg = fade, fg = f },
+    c = { bg = b, fg = f },
+  }
+end
+
+-- stylua: ignore
+local nightfox =  {
+  normal   = generate_mode(pal.blue.base),
+  insert   = generate_mode(pal.green.base),
+  command  = generate_mode(pal.yellow.base),
+  visual   = generate_mode(pal.magenta.base),
+  replace  = generate_mode(pal.red.base),
+  inactive = generate_mode(spec.fg3),
+}
+
+return nightfox
