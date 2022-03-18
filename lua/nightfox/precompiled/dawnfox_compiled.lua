milanglacier/nightfox.nvim-1

feat(modules): Add modes.nvim support

lua/nightfox/precompiled/dawnfox_compiled.lua
@@ -118,6 +118,10 @@ highlight LspTroubleNormal guifg=#a8a3b3 guibg=#ebe5df gui=NONE guisp=NONE |
 highlight LspTroubleText guifg=#625c87 guibg=NONE gui=NONE guisp=NONE |
 highlight MatchParen guifg=#ea9d34 guibg=NONE gui=bold guisp=NONE |
 highlight ModeMsg guifg=#ea9d34 guibg=NONE gui=bold guisp=NONE |
+highlight ModesCopy guifg=NONE guibg=#bdc8bd gui=NONE guisp=NONE |
+highlight ModesDelete guifg=NONE guibg=#debabf gui=NONE guisp=NONE |
+highlight ModesInsert guifg=NONE guibg=#b8cece gui=NONE guisp=NONE |
+highlight ModesVisual guifg=NONE guibg=#d0c3d2 gui=NONE guisp=NONE |
 highlight MoreMsg guifg=#286983 guibg=NONE gui=bold guisp=NONE |
 highlight MsgArea guifg=#625c87 guibg=NONE gui=NONE guisp=NONE |
 highlight NeogitBranch guifg=#ea9d34 guibg=NONE gui=NONE guisp=NONE |

lua/nightfox/precompiled/dayfox_compiled.lua
@@ -118,6 +118,10 @@ highlight LspTroubleNormal guifg=#2e537d guibg=#dbdbdb gui=NONE guisp=NONE |
 highlight LspTroubleText guifg=#233f5e guibg=NONE gui=NONE guisp=NONE |
 highlight MatchParen guifg=#ba793e guibg=NONE gui=bold guisp=NONE |
 highlight ModeMsg guifg=#ba793e guibg=NONE gui=bold guisp=NONE |
+highlight ModesCopy guifg=NONE guibg=#b3c2bb gui=NONE guisp=NONE |
+highlight ModesDelete guifg=NONE guibg=#d6b2bc gui=NONE guisp=NONE |
+highlight ModesInsert guifg=NONE guibg=#b8cfd8 gui=NONE guisp=NONE |
+highlight ModesVisual guifg=NONE guibg=#c5b9c9 gui=NONE guisp=NONE |
 highlight MoreMsg guifg=#4d688e guibg=NONE gui=bold guisp=NONE |
 highlight MsgArea guifg=#233f5e guibg=NONE gui=NONE guisp=NONE |
 highlight NeogitBranch guifg=#ba793e guibg=NONE gui=NONE guisp=NONE |

EdenEast/nightfox.nvim

lua/nightfox/precompiled/dawnfox_compiled.lua
@@ -118,6 +118,10 @@ highlight LspTroubleNormal guifg=#a8a3b3 guibg=#ebe5df gui=NONE guisp=NONE |
 highlight LspTroubleText guifg=#625c87 guibg=NONE gui=NONE guisp=NONE |
 highlight MatchParen guifg=#ea9d34 guibg=NONE gui=bold guisp=NONE |
 highlight ModeMsg guifg=#ea9d34 guibg=NONE gui=bold guisp=NONE |
+highlight ModesCopy guifg=NONE guibg=#ea9d34 gui=NONE guisp=NONE |
+highlight ModesDelete guifg=NONE guibg=#b4637a gui=NONE guisp=NONE |
+highlight ModesInsert guifg=NONE guibg=#56949f gui=NONE guisp=NONE |
+highlight ModesVisual guifg=NONE guibg=#907aa9 gui=NONE guisp=NONE |
 highlight MoreMsg guifg=#286983 guibg=NONE gui=bold guisp=NONE |
 highlight MsgArea guifg=#625c87 guibg=NONE gui=NONE guisp=NONE |
 highlight NeogitBranch guifg=#ea9d34 guibg=NONE gui=NONE guisp=NONE |

lua/nightfox/precompiled/dayfox_compiled.lua
@@ -118,6 +118,10 @@ highlight LspTroubleNormal guifg=#2e537d guibg=#dbdbdb gui=NONE guisp=NONE |
 highlight LspTroubleText guifg=#233f5e guibg=NONE gui=NONE guisp=NONE |
 highlight MatchParen guifg=#ba793e guibg=NONE gui=bold guisp=NONE |
 highlight ModeMsg guifg=#ba793e guibg=NONE gui=bold guisp=NONE |
+highlight ModesCopy guifg=NONE guibg=#ba793e gui=NONE guisp=NONE |
+highlight ModesDelete guifg=NONE guibg=#b95d76 gui=NONE guisp=NONE |
+highlight ModesInsert guifg=NONE guibg=#6ca7bd gui=NONE guisp=NONE |
+highlight ModesVisual guifg=NONE guibg=#8e6f98 gui=NONE guisp=NONE |
 highlight MoreMsg guifg=#4d688e guibg=NONE gui=bold guisp=NONE |
 highlight MsgArea guifg=#233f5e guibg=NONE gui=NONE guisp=NONE |
 highlight NeogitBranch guifg=#ba793e guibg=NONE gui=NONE guisp=NONE |