b882c7b9128c13cb57693b48bec24bfb140bbf7a
Leon Connor Holm <leonconnorholm@gmail.com>
2023-02-13 13:54:25 +0100

    added init with supported modules table

---
+++ lua/nightfox/group/modules/init.lua
@@ -0,0 +1,41 @@
+local M = {
+    "aerial",
+    "barbar",
+    "cmp",
+    "coc",
+    "dap_ui",
+    "dashboard",
+    "diagnostic",
+    "fern",
+    "fidget",
+    "gitgutter",
+    "gitsigns",
+    "glyph_palette",
+    "hop",
+    "illuminate",
+    "lightspeed",
+    "lsp_saga",
+    "lsp_trouble",
+    "mini",
+    "modes",
+    "native_lsp",
+    "navic",
+    "neogit",
+    "neotest",
+    "neotree",
+    "notify",
+    "nvimtree",
+    "pounce",
+    "signify",
+    "sneak",
+    "symbol_outline",
+    "telescope",
+    "treesitter",
+    "tsrainbow",
+    "tsrainbow2",
+    "whichkey"
+}
+
+
+
+return M
