3be1ee3f1ed1570952bf3575b5226145f5ca2d15
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-23 18:50:13 +0700

    feat(semantic_tokens): add injected highlights

--- lua/nightfox/group/modules/lsp_semantic_tokens.lua
+++ lua/nightfox/group/modules/lsp_semantic_tokens.lua
@@ -14,7 +14,10 @@ function M.get(spec, config, opts)
     ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
     ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
     ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.operator.injected"] = { link = "@operator" },
     ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
+    ["@lsp.typemod.string.injected"] = { link = "@string" },
+    ["@lsp.typemod.variable.injected"] = { link = "@variable" },
   }
 end
 
