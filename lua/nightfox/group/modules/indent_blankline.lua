b2d6e2d868b5c20f6a50ae78a68b714f7b897951
nullchilly <nullchilly@gmail.com>
2023-05-12 20:30:33 +0700

    fix(indent_blankline): style wasn't set correctly

--- lua/nightfox/group/modules/indent_blankline.lua
+++ lua/nightfox/group/modules/indent_blankline.lua
@@ -5,7 +5,7 @@ function M.get(spec, config, opts)
   return {
     IndentBlanklineChar = { fg = spec.bg3 },
     IndentBlanklineContextChar = { fg = spec.syntax.variable },
-    IndentBlanklineContextStart = { sp = spec.syntax.variable, underline = true },
+    IndentBlanklineContextStart = { sp = spec.syntax.variable, style = "underline" },
     IndentBlanklineIndent6 = { blend = 0, fg = c.yellow.base },
     IndentBlanklineIndent5 = { blend = 0, fg = c.red.base },
     IndentBlanklineIndent4 = { blend = 0, fg = c.cyan.base },
