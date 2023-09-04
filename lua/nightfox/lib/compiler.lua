31a088af7469fa39383cedc9afe1e2ec0bd4fded
EdenEast <edenofest@gmail.com>
2023-09-04 15:16:29 -0400

    fix(compiler): add file permission error

--- lua/nightfox/lib/compiler.lua
+++ lua/nightfox/lib/compiler.lua
@@ -67,14 +67,27 @@ vim.o.background = "%s"
   local output_path, output_file = config.get_compiled_info(opts)
   util.ensure_dir(output_path)
 
-  local file
+  local file, err
   if vim.g.nightfox_debug then
     file = io.open(output_file .. ".lua", "wb")
     file:write(table.concat(lines, "\n"))
     file:close()
   end
 
-  file = io.open(output_file, "wb")
+  file, err = io.open(output_file, "wb")
+  if not file then
+    require("nightfox.lib.log").error(fmt(
+      [[Couldn't open %s: %s.
+
+Check that %s is accessible for the current user.
+You could try deleting %s to reset permissions]],
+      output_file,
+      err,
+      output_file,
+      output_path
+    ))
+    return
+  end
 
   local f = loadstring(table.concat(lines, "\n"), "=")
   if not f then
