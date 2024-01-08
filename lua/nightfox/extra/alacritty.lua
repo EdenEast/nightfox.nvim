83bad1576d94251d460b2cc5635a15dd73cf6f65
EdenEast <edenofest@gmail.com>
2024-01-08 14:10:03 -0500

    fix(extra): alacritty config from yml to toml (#400)
    * fix(extra): switch from yml to toml
    
    Alacritty 0.13 changed config file from yml to toml.
    
    * chore(extra): generate extra files
    
    ---------
    
    Co-authored-by: EdenEast <EdenEast@users.noreply.github.com>

--- lua/nightfox/extra/alacritty.lua
+++ lua/nightfox/extra/alacritty.lua
@@ -2,71 +2,93 @@ local template = require("nightfox.util.template")
 
 local M = {}
 
-local function conv(color)
-  return color:gsub("^#", "0x")
-end
-
-local function construct(shade)
-  return {
-    base = conv(shade.base),
-    bright = conv(shade.bright),
-  }
-end
-
 function M.generate(spec, _)
-  -- Convert default hex string format to Alacritty `0x` format
-  local p = spec.palette
-  local colors = {
-    meta = p.meta,
-    bg = conv(spec.bg1),
-    fg = conv(spec.fg1),
-    black = construct(p.black),
-    red = construct(p.red),
-    green = construct(p.green),
-    yellow = construct(p.yellow),
-    blue = construct(p.blue),
-    magenta = construct(p.magenta),
-    cyan = construct(p.cyan),
-    white = construct(p.white),
-    orange = construct(p.orange),
-    pink = construct(p.pink),
-  }
+  spec.dim_fg = spec.palette.meta.light and spec.fg0 or spec.fg2
+  spec.bright_fg = spec.palette.meta.light and spec.fg2 or spec.fg0
 
-  local content = [[
+  local content = [===[
 # Nightfox Alacritty Colors
-# Style: ${meta.name}
-# Upstream: ${meta.url}
-colors:
-  # Default colors
-  primary:
-    background: '${bg}'
-    foreground: '${fg}'
-  # Normal colors
-  normal:
-    black:   '${black}'
-    red:     '${red}'
-    green:   '${green}'
-    yellow:  '${yellow}'
-    blue:    '${blue}'
-    magenta: '${magenta}'
-    cyan:    '${cyan}'
-    white:   '${white}'
-  # Bright colors
-  bright:
-    black:   '${black.bright}'
-    red:     '${red.bright}'
-    green:   '${green.bright}'
-    yellow:  '${yellow.bright}'
-    blue:    '${blue.bright}'
-    magenta: '${magenta.bright}'
-    cyan:    '${cyan.bright}'
-    white:   '${white.bright}'
-  indexed_colors:
-    - { index: 16, color: '${orange}' }
-    - { index: 17, color: '${pink}' }
-]]
+## name: ${palette.meta.name}
+## upstream: ${palette.meta.url}
 
-  return template.parse_template_str(content, colors)
+[colors.primary]
+background = "${bg1}"
+foreground = "${fg1}"
+dim_forground = "${dim_fg}"
+bright_forground = "${bright_fg}"
+
+[colors.cursor]
+text = "${fg1}"
+cursor = "${fg2}"
+
+[colors.vi_mode_cursor]
+text = "${fg1}"
+cursor = "${palette.cyan.base}"
+
+[colors.search.matches]
+foreground = "${fg1}"
+background = "${sel1}"
+
+[colors.search.focused_match]
+foreground = "${fg1}"
+background = "${diag.hint}"
+
+[colors.footer_bar]
+foreground = "${fg1}"
+background = "${bg3}"
+
+[colors.hints.start]
+foreground = "${fg1}"
+background = "${palette.orange.base}"
+
+[colors.hints.end]
+foreground = "${fg1}"
+background = "${bg3}"
+
+[colors.selection]
+text = "${fg1}"
+background = "${sel0}"
+
+[colors.normal]
+black = "${palette.black.base}"
+red = "${palette.red.base}"
+green = "${palette.green.base}"
+yellow = "${palette.yellow.base}"
+blue = "${palette.blue.base}"
+magenta = "${palette.magenta.base}"
+cyan = "${palette.cyan.base}"
+white = "${palette.white.base}"
+
+[colors.bright]
+black = "${palette.black.bright}"
+red = "${palette.red.bright}"
+green = "${palette.green.bright}"
+yellow = "${palette.yellow.bright}"
+blue = "${palette.blue.bright}"
+magenta = "${palette.magenta.bright}"
+cyan = "${palette.cyan.bright}"
+white = "${palette.white.bright}"
+
+[colors.dim]
+black = "${palette.black.dim}"
+red = "${palette.red.dim}"
+green = "${palette.green.dim}"
+yellow = "${palette.yellow.dim}"
+blue = "${palette.blue.dim}"
+magenta = "${palette.magenta.dim}"
+cyan = "${palette.cyan.dim}"
+white = "${palette.white.dim}"
+
+[[colors.indexed_colors]]
+index = 16
+color = "${palette.orange.base}"
+
+[[colors.indexed_colors]]
+index = 17
+color = "${palette.pink.base}"
+]===]
+
+  return template.parse_template_str(content, spec)
 end
 
 return M
